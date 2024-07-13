943436d253b8da4940c64cf0f2aee5711b0c49b5
Swapndar <swapnil.darshanale@amdocs.com>
2024-07-13 16:38:30 +0530

    sing in page added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  let [isLoginFrom, SetLoginForm] = useState(true);
+  let toggleSingInForm = () => {
+    SetLoginForm(!isLoginFrom);
+  };
   return (
     <div className="h-4">
       <Header />
@@ -11,11 +15,36 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className="p-12 bg-black absolute mx-auto left-0 right-0 my-36 w-3/12 text-white bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sing In</h1>
-        <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700" />
-        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700" />
-        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
+
+      <form className="p-12 bg-black absolute mx-auto left-0 right-0 my-36 w-3/12 text-white bg-opacity-80 rounded-xl">
+        <h1 className="font-bold text-3xl py-4">
+          {isLoginFrom ? "Sing In" : "Sing Up"}
+        </h1>
+        {!isLoginFrom && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          ></input>
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          {isLoginFrom ? "Sing In" : "Sing Up"}
+        </button>
+        <p className="p-4 cursor-pointer" onClick={toggleSingInForm}>
+          {isLoginFrom
+            ? "New to Netflix? Sign up now"
+            : "Already Registered ?Sing In Now... "}
+        </p>
       </form>
     </div>
   );
